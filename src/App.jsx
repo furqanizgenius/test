850e1fd75d75999140ae80e90d4273b2183f58f8
Furqan <furqanizgenius@gmail.com>
2025-08-10 22:54:15 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
+import './App.css';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Error from './components/Error'
-import { Router, Route, Routes, BrowserRouter, Link } from 'react-router-dom'
+import Nav from './components/Nav';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
 function App() {
-return (
-  <>
-    <Router>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
+  return (
+    <>
+      <BrowserRouter>
+        <Nav />
 
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="#" element={<Error/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="#" element={<Error />} />
         </Routes>
-      </div>
-    </Router>
-  </>
-)
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
